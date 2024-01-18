dae71104e06dcd111b220c969d473f09a174c5f6
Adam Eger <egea00@vse.cz>
2024-01-18 20:07:58 +0100

    Update serverless function configurations

--- api/ticketmaster.js
+++ api/ticketmaster.js
@@ -2,11 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
     const proxy = createProxyMiddleware({
-        target: 'https://app.ticketmaster.com',
-        changeOrigin: true,
-        pathRewrite: {
-            '^/ticketmaster': '',
-        },
+        target: 'https://api.openweathermap.org',
+        changeOrigin: true
     });
     return proxy(req, res);
 };
